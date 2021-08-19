c86dd5bc2a784f786df5e5b8a9db6873b80810bb
RKostochko <kostochko.tech@mail.ru>
2021-08-19 11:14:15 +0300

    fead: task complete

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,67 @@
 class TicTacToe {
-    constructor() {
+	constructor() {
+		this.currentSymbol = 'x';
+		this.playground = [
+			[null, null, null],
+			[null, null, null],
+			[null, null, null],
+		];
+		this.winComb = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+	}
 
-    }
+	// возращает х и 0 (нынешний символ)
+	getCurrentPlayerSymbol() {
+		return this.currentSymbol;
+	}
 
-    getCurrentPlayerSymbol() {
+	// обновляет состояние класса (изменить текущего игрока, обновить хранилище меток)
+	nextTurn(rowIndex, columnIndex) {
+		if (this.playground[rowIndex][columnIndex] === null) {
+			this.playground[rowIndex][columnIndex] = this.currentSymbol;
+			this.currentSymbol = (this.currentSymbol === 'x' ? 'o' : 'x');
+		}
+	}
 
-    }
+	// возвращает истину, если игра окончена (победа или ничья)
+	isFinished() {
+		return Boolean(this.getWinner()) || this.isDraw();
+	}
 
-    nextTurn(rowIndex, columnIndex) {
+	// возвращает символ победителя или null, если его нет
+	getWinner() {
+		const playgroundArr = this.playground.flat();
+		for (let i = 0; i < this.winComb.length; i++) {
+			const [a, b, c] = this.winComb[i];
+			if (playgroundArr[a] === playgroundArr[b] && playgroundArr[a] === playgroundArr[c]) {
+				return playgroundArr[a];
+			}
+		}
+		return null;
+	}
 
-    }
+	// возвращает истину, если полей для размещения нет
+	noMoreTurns() {
+		return Boolean(this.playground.flat().indexOf(null) === -1);
+	}
 
-    isFinished() {
+	// возвращает истину, если закончились ходы и нет победителя
+	isDraw() {
+		return Boolean(this.noMoreTurns() && !this.getWinner());
+	}
 
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+	// возращает matrix[row][col] или null
+	getFieldValue(rowIndex, colIndex) {
+		return this.playground[rowIndex][colIndex];
+	}
 }
 
 module.exports = TicTacToe;
